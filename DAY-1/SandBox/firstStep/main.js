69ff399e4dbc8eda63b9f0eef1f2a923b2e1d53d
Jules_Dev <julesloic9@gmail.com>
2025-04-14 17:56:54 +0200

    Appel des fonctions exporter dans math.js

---
+++ DAY-1/SandBox/firstStep/main.js
@@ -0,0 +1,6 @@
+import { addition, soustraction, multiplication, division } from "./math.js";
+
+console.log(`La somme de 4 et 2 est ${addition(4,2)}`)
+console.log(`La sustraction de 4 par 2 est ${soustraction(4,2)}`)
+console.log(`La multiplication de 4 par 2 est ${multiplication(4,2)}`)
+console.log(`La division de 4 par 2 est ${division(4,2)}`)
