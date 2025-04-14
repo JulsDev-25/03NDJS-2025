c5f94b6a829c30e719d262ba0ffc8c231712d671
JulsDev-25 <julesloic9@gmail.com>
2025-04-14 12:32:34 +0200

    Initialisation

---
+++ DAY-1/SandBox/boucleFor.js
@@ -0,0 +1,12 @@
+// //Exemple de bouble for
+// for (let i=0; i<11; i++) {
+//     console.log(i);
+// }
+
+//exemple de boucle do.. wille
+let i = 0;
+do {
+    i += 1;
+    console.log(i);
+    
+} while (i<5)
