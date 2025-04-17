1dece02b3bf1fcf0b115886191d63bddd310e33c
Jules_Dev <julesloic9@gmail.com>
2025-04-18 00:55:54 +0200

    Api GET/users et DELETE/users/:id

--- Day-4/exo/project-root/controlleurs/userController.js
+++ Day-4/exo/project-root/controlleurs/userController.js
@@ -81,4 +81,37 @@ export const me = (req, res) => {
       res.status(404).json({ message: 'User not found' });
     }
   };
+
+// GET/ users
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private
+export const getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find().select('-password'); // exclut les mots de passe
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+
+// @desc    Delete a user by ID
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+export const deleteUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+    
+  
   
