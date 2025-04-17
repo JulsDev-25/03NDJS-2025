896724063afa6d15d6d0d77e87c659205391edd4
Jules_Dev <julesloic9@gmail.com>
2025-04-17 21:13:34 +0200

    Route post/register

--- Day-4/exo/project-root/routes/userRoutes.js
+++ Day-4/exo/project-root/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import registerUser from '../controlleurs/userController.js'
+import {registerUser} from '../controlleurs/userController.js'
 
 const router = express.Router();
 
